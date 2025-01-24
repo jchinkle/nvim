87d9569c8694b395c849b70e8f7c5cc24d03e8de
Jeremy Hinkle <jchinkle@gmail.com>
2025-01-24 12:02:55 -0600

    refactor(plugins): replace fzf.vim with telescope.nvim for better integration and features

--- lua/slydragonn/plugins/telescope.lua
+++ lua/slydragonn/plugins/telescope.lua
@@ -1,22 +1,37 @@
 return {
 	{
-		'junegunn/fzf.vim',
-		dependencies = { 'junegunn/fzf' },
+		'nvim-telescope/telescope.nvim',
+		tag = '0.1.6',
+		dependencies = { 'nvim-lua/plenary.nvim' },
 		config = function()
-			-- Use fd for faster file finding
-			vim.env.FZF_DEFAULT_COMMAND = 'fd --type f --hidden --follow --exclude .git'
-			
-			-- Basic settings
-			vim.g.fzf_layout = { down = '40%' }
-			vim.g.fzf_preview_window = {}  -- Disable preview for speed
-			vim.g.fzf_buffers_jump = 1
-			
+			local telescope = require('telescope')
+			telescope.setup({
+				defaults = {
+					file_ignore_patterns = { "node_modules", ".git/" },
+					layout_strategy = 'vertical',
+					layout_config = {
+						vertical = { width = 0.8, height = 0.8 }
+					},
+					preview = false -- Disable preview for speed
+				}
+			})
+
+			-- Smart file finder that uses git_files in git repos
+			local function project_files()
+				local opts = {} -- define here if you want to define something
+				local ok = pcall(require'telescope.builtin'.git_files, opts)
+				if not ok then require'telescope.builtin'.find_files(opts) end
+			end
+
 			-- Key mappings
-			vim.keymap.set('n', '<C-p>', ':Files<CR>', { silent = true })
-			vim.keymap.set('n', '<leader>f', ':Files<CR>', { silent = true })
-			vim.keymap.set('n', '<leader>b', ':Buffers<CR>', { silent = true })
-			vim.keymap.set('n', '<leader>g', ':Rg<CR>', { silent = true })
-			vim.keymap.set('n', '<leader>*', ':Rg <C-R><C-W><CR>', { silent = true })
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<C-p>', project_files, { silent = true })
+			vim.keymap.set('n', '<leader>f', project_files, { silent = true })
+			vim.keymap.set('n', '<leader>b', builtin.buffers, { silent = true })
+			vim.keymap.set('n', '<leader>g', builtin.live_grep, { silent = true })
+			vim.keymap.set('n', '<leader>*', function()
+				builtin.grep_string({ search = vim.fn.expand("<cword>") })
+			end, { silent = true })
 		end,
-	},
+	}
 }
