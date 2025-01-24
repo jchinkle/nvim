8283e7c59e0d617a45f9655eb18082115f8035c8
Jeremy Hinkle <jchinkle@gmail.com>
2025-01-24 11:01:21 -0600

    feat(neotree): configure neo-tree and auto-open on startup
    Added setup configuration for neo-tree plugin with customized window and filesystem options. Automatically open the neo-tree when Neovim starts.

--- lua/slydragonn/plugins/neotree.lua
+++ lua/slydragonn/plugins/neotree.lua
@@ -7,4 +7,27 @@ return {
 		"MunifTanjim/nui.nvim",
 		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
+	config = function()
+		require("neo-tree").setup({
+			window = {
+				position = "left",
+				width = 30,
+			},
+			filesystem = {
+				follow_current_file = true,
+				filtered_items = {
+					visible = true,
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+			},
+		})
+		
+		-- Open Neo-tree on startup
+		vim.api.nvim_create_autocmd("VimEnter", {
+			callback = function()
+				require("neo-tree.command").execute({ toggle = true })
+			end,
+		})
+	end,
 }
